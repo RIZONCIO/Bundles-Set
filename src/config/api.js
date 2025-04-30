ae61ff1054fd9d88aa3f65570844bd6011e7f732
Matheus Coelho <matheusvgp29@gmail.com>
2025-04-30 02:25:46 -0300

    fix: update API endpoint definitions to use import.meta.env

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 const API_ENDPOINTS = {
-    BUNDLES: process.env.VITE_API_BUNDLES,
-    BUNDLES_ALL: process.env.VITE_API_BUNDLES_ALL,
-  };
-  
-  export default API_ENDPOINTS;
+  BUNDLES: import.meta.env.VITE_API_BUNDLES,
+  BUNDLES_ALL: import.meta.env.VITE_API_BUNDLES_ALL,
+};
+
+export default API_ENDPOINTS;
