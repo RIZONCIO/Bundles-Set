41fd572e89cde18a70f882ead687f27785aa4f6d
Matheus Coelho <matheusvgp29@gmail.com>
2025-04-30 02:36:33 -0300

    fix: update IndexedDB object store name from "bundles" to "allBundles"

--- src/scripts/main.js
+++
@@ -1,8 +0,0 @@
-import "/displayBundles.js";
-import "/infiniteScroll.js";
-import "/darkmode.js";
-import "/modal18.js";
-import "/carousel.js";
-import "/buscador.js";
-import "/filtros.js";
-import "/footer.js";
